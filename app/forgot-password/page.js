48ca73b31b26379d8f20500460a9730313b29a4d
senemdemirr <senemdemir262@gmail.com>
2025-11-20 14:58:59 +0300

    added default function

---
+++ app/forgot-password/page.js
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function ForgotPassword(){
+    return(
+        <>
+        <Typography variant="h5" className="!text-gray-600"> Forgot Password Page</Typography>
+        </>
+    )
+}
