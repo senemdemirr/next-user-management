c023a5083c14c1ec8cc382511963b09d8f6c89a2
senemdemirr <senemdemir262@gmail.com>
2025-11-21 14:16:23 +0300

    completed account page

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,27 @@
-import { Typography } from "@mui/material";
+"use client";
+import { Typography, Button } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function Account() {
+    const router = useRouter();
+
+    const storedUser = localStorage.getItem("auth_user");
+    if (!storedUser) return null;
+
+    const user = JSON.parse(storedUser);
+
+    function LogOut() {
+        localStorage.removeItem("auth_user");
+        localStorage.removeItem("auth_token");
+        router.push("/login");
+    }
     return (
-        <>
-            <Typography variant="h5" className="!text-gray-600"> Account Page </Typography>
-        </>
+        <div className="flex flex-col justify-center items-center w-screen h-screen">
+            <Typography variant="h6" className="!text-gray-600"> Hello <Typography component="span" className="!text-2xl">{user?.name}</Typography>
+            </Typography>
+            <Button variant="contained" onClick={LogOut} className="!mt-4"> Log out </Button>
+        </div>
+
     )
 }
