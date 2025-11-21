ebe17cb49d7fd07a88c111348689d5228906bb8f
senemdemirr <senemdemir262@gmail.com>
2025-11-21 10:14:59 +0300

    added default function

---
+++ app/account/page.js
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Account() {
+    return (
+        <>
+            <Typography variant="h5" className="!text-gray-600"> Account Page </Typography>
+        </>
+    )
+}
---
+++ app/delete-account/page.js
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function DeleteAccount() {
+    return (
+        <>
+            <Typography variant="h5" className="!text-gray-600"> Delete Account Page </Typography>
+        </>
+    )
+}
