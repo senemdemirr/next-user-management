0316c41aa20972e3f5007a24c6efeee54743de0d
senemdemirr <senemdemir262@gmail.com>
2025-11-23 00:05:02 +0300

    removed unused page

--- app/delete-account/page.js
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function DeleteAccount() {
-    return (
-        <>
-            <Typography variant="h5" className="!text-gray-600"> Delete Account Page </Typography>
-        </>
-    )
-}
